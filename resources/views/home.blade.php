7d7668ad6120dc1b73a8daa5c3a23cb6b61e674d
Anne Noteboom <annenootje@live.nl>
2020-06-30 20:19:08 +0200

    Pages

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title', 'Home')
+
+@section('content')
+
+    <section>
+        <div class="container">
+            Anne noteboom
+        </div>
+    </section>
+
+@endsection
