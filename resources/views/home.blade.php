295cb8576ad22e9795a5b4c10b5e1e6542318974
Anne Noteboom <annenootje@live.nl>
2020-07-01 10:13:40 +0200

    Sass opzet

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,32 @@
 
 @section('content')
 
-    <section>
+    <section class="line">
         <div class="container">
-            Anne noteboom
+            <div class="part-12">
+                <h1 class="large center">
+                    Meer dan gewoon<br>
+                    een <span class="pink">website</span>
+                </h1>
+                <br>
+                <h4 class="blue center">Anne Noteboom, multimediaal ontwerper</h4>
+            </div>
+        </div>
+    </section>
+
+    <section class="line">
+        <div class="container">
+            <div class="part-12">
+                <h2 class="center">Laatste project</h2>
+            </div>
+        </div>
+    </section>
+
+    <section class="line">
+        <div class="container">
+            <div class="part-12">
+                <h2 class="center">Diensten</h2>
+            </div>
         </div>
     </section>
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,10 +6,56 @@
 
         <title>Anne Noteboom</title>
         <link rel="stylesheet" href="/css/app.css">
+        <link rel="icon" type="image/png" href="/img/favicon.png" />
     </head>
     <body>
 
+        <header>
+            <div class="container">
+                <a href="/" class="logo">
+                    <img src="/img/logo.svg" alt="Logo Anne Noteboom">
+                </a>
+                <nav>
+                    <a href="/about">Over mij</a>
+                    <a href="/services">Diensten</a>
+                    <a href="/projects">Projecten</a>
+                    <a href="/contact" class="button">Contact</a>
+                </nav>
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </header>
+
         @yield('content')
+
+        <footer>
+            <div class="container">
+                <div class='part-3'>
+                    <div class="logo">
+                        <img src="/img/logo.svg" alt="Logo Anne Noteboom">
+                    </div>
+                </div>
+                <div class='part-5'>
+                    <div class="container">
+                        <div class="part-6">
+                            <p><b>Pagina's</b></p>
+                        </div>
+                        <div class="part-6">
+                            <p><b>Documenten</b></p>
+                            <a href="/privacy">Privacy verklaring</a>
+                            <a href="/cookies">Cookies</a>
+                        </div>
+                    </div>
+                </div>
+                <div class='part-3 right-content'>
+                    <p><b>Contact</b></p>
+                    <a href="mailto:info@annenoteboom.nl">info@annenoteboom.nl</a>
+                </div>
+            </div>
+        </footer>
         
     </body>
 </html>
